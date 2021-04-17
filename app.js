e8de74b6c7009f0424a07641aa52ce7f64283e05
cyrialize <jonathancyr@fastmail.com>
2021-04-17 13:31:10 -0400

    Completed lesson 84

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(shopRoutes);
 
 // Works as a catch all since all other routes are inserted above
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 app.listen(3000);
