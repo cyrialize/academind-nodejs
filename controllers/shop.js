d0c3c11b1b4ee24b51d960685bd6730b01532e0d
cyrialize <jonathancyr@fastmail.com>
2021-04-29 19:49:28 -0400

    Completed lesson 120

--- controllers/shop.js
+++ controllers/shop.js
@@ -19,7 +19,9 @@ exports.getProducts = (req, res, next) => {
 // productId matches the name given in the dynamic route, :productId
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    console.log(prodId);
+    Product.findById(prodId, product => {
+        console.log(product);
+    });
     res.redirect('/');
 };
 
