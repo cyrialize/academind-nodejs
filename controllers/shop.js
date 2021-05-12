c5ec80e969f7ada98afc726345f21c6848c20394
cyrialize <jonathancyr@fastmail.com>
2021-05-11 20:25:26 -0400

    Cleaned up controllers/shop.js, made it match course

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,27 +2,20 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-
-    const products = Product.fetchAll((products) => {
-        // Previous way of doing it, sending an html file as opposed to a template
-        // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-        // 
-        // res.render() uses the templating engine defined by app.set('view_engine', '');
-        // The second argument of the render function is a JS object with key->value pairs to pass data into the view
+    Product.fetchAll(products => {
         res.render('shop/product-list', {
-            prods: products, 
-            pageTitle: 'All Products', 
-            path: '/products', 
+            prods: products,
+            pageTitle: 'All Products',
+            path: '/products'
         });
     });
 };
 
-// productId matches the name given in the dynamic route, :productId
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
     Product.findById(prodId, product => {
         res.render('shop/product-detail', {
-            product: product, 
+            product: product,
             pageTitle: product.title,
             path: '/products'
         });
@@ -30,28 +23,50 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
+    Product.fetchAll(products => {
         res.render('shop/index', {
-            prods: products, 
-            pageTitle: 'Shop', 
-            path: '/', 
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/'
         });
     });
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(
+                    prod => prod.id === product.id
+                );
+                if (cartProductData) {
+                    cartProducts.push({ productData: product, qty: cartProductData.qty });
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        });
     });
 };
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, (product) => {
+    Product.findById(prodId, product => {
         Cart.addProduct(prodId, product.price);
     });
     res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price);
+        res.redirect('/cart');
+    });
 };
 
 exports.getOrders = (req, res, next) => {
